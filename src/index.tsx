fd8d31d61389756660e38c6e9919a06cd7e5a9bc
igor.kudlo <igoreklida@gmail.com>
2024-04-02 18:41:14 +0300

    main page layout

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { store } from '@redux/configure-store';
 import { MainPage } from './pages';
 
 import 'normalize.css';
+import 'antd/dist/antd.css';
 import './index.css';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,34 +1,107 @@
 import React, { useState } from 'react';
-
-import reactLogo from '/react.svg';
-import viteLogo from '/vite.svg';
-import tsLogo from '/ts.svg';
+import { Button, Card, Layout, Menu } from 'antd';
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    LogoutOutlined,
+    CalendarOutlined,
+    HeartFilled,
+    TrophyFilled,
+    AuditOutlined,
+    SettingOutlined,
+    AndroidFilled,
+    AppleFilled
+} from '@ant-design/icons';
+import fitLogo from '/fit.svg';
 import './main-page.css';
 
+const { Header, Footer, Sider, Content } = Layout;
+
+const menuItems = [
+    {
+        key: '1',
+        icon: <CalendarOutlined />,
+        label: 'Календарь',
+    },
+    {
+        key: '2',
+        icon: <HeartFilled />,
+        label: 'Тренировки',
+    },
+    {
+        key: '3',
+        icon: <TrophyFilled />,
+        label: 'Достижения',
+    },
+    {
+        key: '4',
+        icon: <AuditOutlined />,
+        label: 'Профиль',
+    }
+];
+
 export const MainPage: React.FC = () => {
-    const [count, setCount] = useState(0);
+    const [collapsed, setCollapsed] = useState(false);
 
     return (
-        <>
-            <div>
-                <a href='https://vitejs.dev' target='_blank'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank'>
-                    <img src={reactLogo} className='logo react' alt='React logo' />
-                </a>
-                <a href='https://www.typescriptlang.org/' target='_blank'>
-                    <img src={tsLogo} className='logo' alt='TS logo' />
-                </a>
-            </div>
-            <h1>Vite + React + TS</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/pages/main-page.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-        </>
+        <Layout className="layout">
+            <Sider className="sider" trigger={null} collapsible collapsed={collapsed} theme='light'>
+                <img className="logo" src={fitLogo} alt="Fit logo"/>
+                <Menu
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={menuItems}
+                />
+                <div className="sider-trigger" onClick={() => setCollapsed(!collapsed)}>
+                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
+                </div>
+                <Button className="exit-button" type="link" block icon={<LogoutOutlined />}>Выход</Button>
+            </Sider>
+            <Layout>
+                <Header className="header">
+                    <div className="page-title">Главная</div>
+                    <div className="header-col">
+                        <p className="main-title">Приветствуем тебя в Fit — приложении, которое поможет тебе добиться своей
+                            мечты!</p>
+                        <Button className="settings-button" type="link" icon={<SettingOutlined />}>Настройки</Button>
+                    </div>
+                </Header>
+                <Content>
+                    <div className="main-content-inner">
+                        <Card className="card-regular" bordered={false}>
+                            <div>С CleverFit ты сможешь:</div>
+                            <div>— планировать свои тренировки на календаре, выбирая тип и уровень нагрузки;</div>
+                            <div>— отслеживать свои достижения в разделе статистики, сравнивая свои результаты с нормами и рекордами;</div>
+                            <div>— создавать свой профиль, где ты можешь загружать свои фото, видео и отзывы о тренировках;</div>
+                            <div>— выполнять расписанные тренировки для разных частей тела, следуя подробным инструкциям и советам профессиональных тренеров.</div>
+                        </Card>
+                        <Card className="card-bold" bordered={false}>
+                            <div>CleverFit — это не просто приложение, а твой личный помощник в мире фитнеса. Не откладывай на завтра — начни тренироваться уже сегодня!</div>
+                        </Card>
+                        <div className="cards-col">
+                            <Card title="Расписать тренировки" bordered={false} className="card-with-title">
+                                <a href="#" className="card-link"><HeartFilled /> Тренировки</a>
+                            </Card>
+                            <Card title="Назначить календарь" bordered={false} className="card-with-title">
+                                <a href="#" className="card-link"><CalendarOutlined /> Календарь</a>
+                            </Card>
+                            <Card title="Заполнить профиль" bordered={false} className="card-with-title">
+                                <a href="#" className="card-link"><AuditOutlined /> Профиль</a>
+                            </Card>
+                        </div>
+                    </div>
+                </Content>
+                <Footer>
+                    <a href="#">Смотреть отзывы</a>
+                    <Card title={<><div style={{ color: '#2f54eb' }}>Скачать на телефон</div><div style={{ color: '#8c8c8c' }}>Доступно в PRO-тарифе</div></>}
+                          bordered={false} className="card-devider">
+                        <div className="card-devider__body">
+                            <a href="#"><AndroidFilled/> Android OS</a>
+                            <a href="#"><AppleFilled/> Apple iOS</a>
+                        </div>
+                    </Card>
+                </Footer>
+            </Layout>
+        </Layout>
     );
 };
